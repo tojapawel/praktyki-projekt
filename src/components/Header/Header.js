3bd95a01dccc199933c8fd0de583a64c13718c95
tojapawel <pawel.kozicki01@.com>
2023-06-19 17:42:33 +0000

    dodane sortowanie

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,8 @@ const Header = (props) => {
 	const [city, setCity] = useState('');
 	const [stars, setStars] = useState('');
 
+	const [sort, setSort] = useState('');
+
 	useEffect(() => {
 		const filtered = props.hotels.filter((hotel) => {
 			if (city && hotel.location.city.toLocaleLowerCase() !== city.toLocaleLowerCase()) {
@@ -19,8 +21,31 @@ const Header = (props) => {
 			return true;
 		});
 
+		switch (sort) {
+			case "gm":
+				filtered.sort((a, b) => b.stars - a.stars);
+				break;
+			case "gr":
+				filtered.sort((a, b) => a.stars - b.stars);
+				break;
+			case "odcm":
+				filtered.sort((a, b) => b.metadata.distanceFromCenter - a.metadata.distanceFromCenter);
+				break;
+			case "odcr":
+				filtered.sort((a, b) => a.metadata.distanceFromCenter - b.metadata.distanceFromCenter);
+				break;
+			case "na":
+				filtered.sort((a, b) => a.name.localeCompare(b.name));
+				break;
+			case "ma":
+				filtered.sort((a, b) => a.location.city.localeCompare(b.location.city));
+				break;
+			default:
+				break;
+		}
+
 		props.fiHotels(filtered);
-	}, [city, stars, props.hotels]);
+	}, [sort, city, stars, props.hotels]);
 
 	return (
 		<div className={styles.header}>
@@ -36,6 +61,25 @@ const Header = (props) => {
 				<option value="4">4</option>
 				<option value="5">5</option>
 			</select>
+
+			<br />
+
+			<label htmlFor='sort-select'>Sortowanie: </label>
+			<select id="sort-select" value={sort} onChange={(e) => setSort(e.target.value)} >
+				<option value="">Wybierz sortowanie</option>
+				<option value="" disabled>----------------------------</option>
+				<option value="gm">Gwiazdki malejąco</option>
+				<option value="gr">Gwiazdki rosnąco</option>
+				<option value="" disabled>----------------------------</option>
+				<option value="odcm">Odległość do centrum malejąco</option>
+				<option value="odcr">Odległość do centrum rosnąco</option>
+				<option value="" disabled>----------------------------</option>
+				<option value="na">Nazwa alfabetycznie</option>
+				<option value="" disabled>----------------------------</option>
+				<option value="ma">Miasto alfabetycznie</option>
+				<option value="" disabled>----------------------------</option>
+			</select>
+
 		</div>
 	);
 };
