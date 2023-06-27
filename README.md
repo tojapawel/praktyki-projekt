58c261de0d78b9b345a697f01058aaf31f3ec042
tojapawel <pawel.kozicki01@.com>
2023-06-27 18:51:19 +0000

    przeniesienie wszystkeigo na dockera, dodanie silnika bazdy danych i api do tego

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# praktyki-projekt
-Kozicki Paweł
+# praktyki-projekt - (hotel-finder)
+
+autor: Kozicki Paweł
+
+### uruchomienie
+
+uruchonmienie dockera:
+
+```bash
+docker-compose up -d
+```
 
 #### Podgląd projektu w czasie rzeczywistym
+
 [Link do strony](http://hassioustka.duckdns.org:3354/)
---
+++ api-server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3001;
+
+// MySQL connection configuration
+const connection = mysql.createConnection({
+  host: '192.168.0.214',
+  user: 'root',
+  password: 'root',
+  database: 'hotel-finder',
+  port: 33075
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL database');
+});
+
+// Middleware for parsing JSON requests
+app.use(express.json());
+
+// GET endpoint to fetch data from the database
+app.get('/data', (req, res) => {
+  const query = 'SELECT * FROM users';
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query:', error);
+      res.status(500).json({ error: 'Error retrieving data from database' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
